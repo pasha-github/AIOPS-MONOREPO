3c5db8526670711c93bcb41646763ac3236df6db
Hassaan9289 <hassaan.jahangir@royalcyber.com>
2026-04-02 00:20:28 +0500

    added db url encoding

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from database.models import *  # This imports all models so they are registered with SQLModel.metadata  # noqa: F403
+from utils.db_url import encode_database_url_password
 
 load_dotenv()
 
@@ -24,7 +25,9 @@ if config.config_file_name is not None:
 target_metadata = SQLModel.metadata
 
 # Set the sqlalchemy.url from the environment variable or use a default
-db_url = os.getenv("MAIN_SERVER_DATABASE_URL", "sqlite:///agent_management.db")
+db_url = encode_database_url_password(
+    os.getenv("MAIN_SERVER_DATABASE_URL", "sqlite:///agent_management.db")
+)
 config.set_main_option("sqlalchemy.url", db_url)
 
 # other values from the config, defined by the needs of env.py,
