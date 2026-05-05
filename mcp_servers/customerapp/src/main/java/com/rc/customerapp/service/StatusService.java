575ba5206b0ddb2285e4ecb624a40afe05de32f3
UmairShahid <umair.shahid@royalcyber.com>
2026-05-06 00:26:59 +0500

    index page added, datadog call with http

--- mcp_servers/customerapp/src/main/java/com/rc/customerapp/service/StatusService.java
+++ mcp_servers/customerapp/src/main/java/com/rc/customerapp/service/StatusService.java
@@ -1,5 +1,7 @@
 package com.rc.customerapp.service;
 
+import com.rc.customerapp.config.DatadogLogger;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +9,9 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class StatusService {
+    private final DatadogLogger datadogLogger;
 
     public enum Status {
         START, STOP, IDLE
@@ -18,6 +22,9 @@ public class StatusService {
     public void setStart() {
         globalStatus.set(Status.START);
         log.info("Status changed to start");
+        datadogLogger.send(
+                "Status changed to start","info"
+        );
     }
 
     public void setStop() {
@@ -25,11 +32,17 @@ public class StatusService {
 
         if (oldStatus == Status.START) {
             log.error("Status changed from start to stop");
+            datadogLogger.send(
+                    "Status changed from start to stop","error"
+            );
         }
     }
 
     public Status getStatus() {
         log.info("Current status: "+globalStatus.toString());
+        datadogLogger.send(
+                "Current status: "+globalStatus,"info"
+        );
         return globalStatus.get();
 
     }
--- mcp_servers/customerapp/src/test/java/com/rc/customerapp/CustomerappApplicationTests.java
+++ mcp_servers/customerapp/src/test/java/com/rc/customerapp/CustomerappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rc.customerapp;
+/*package com.rc.customerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class CustomerappApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
